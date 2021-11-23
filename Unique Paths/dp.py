bbf867449300c9c1d5270017877ab42a022b0046
redmorr <19555512+redmorr@users.noreply.github.com>
2021-11-23 19:21:12 +0100

    Add dynamic programming solution

---
+++ Unique Paths/dp.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1 for _ in range(n)] for _ in range(m)]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[m - 1][n - 1]
+
+
+    # Redeclared method from above with better memory complexity to compare code
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1 for _ in range(n)]
+
+        for _ in range(1, m):
+            for j in range(1, n):
+                dp[j] = dp[j] + dp[j - 1]
+        return dp[n - 1]
