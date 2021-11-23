8b9d262e0c32a9592e5c11f06b75de81fa12c47f
redmorr <19555512+redmorr@users.noreply.github.com>
2021-11-23 18:04:12 +0100

    Simplify base cases and add tests

--- Unique Paths/dp.py
+++ Unique Paths/dp.py
@@ -3,17 +3,11 @@ from functools import lru_cache
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return self.dp(m -1, n-1)
+        return self.__dp(m - 1, n - 1)
 
     @lru_cache(maxsize=None)
-    def dp(self, i, j):
+    def __dp(self, i, j):
         if i > 0 and j > 0:
-            return self.dp(i - 1, j) + self.dp(i, j - 1)
-        if i > 0:
-            return self.dp(i - 1, 0)
-        if j > 0:
-            return self.dp(0, j - 1)
-        if i == 0 and j == 0:
+            return self.__dp(i - 1, j) + self.__dp(i, j - 1)
+        if i == 0 or j == 0:
             return 1
-
-print(Solution().dp(100, 100))
---
+++ Unique Paths/test.py
@@ -0,0 +1,19 @@
+import pytest
+
+import dp
+
+testcases = [(28, 3, 7),
+             (2, 2, 2),
+             (3, 3, 2),
+             (6, 3, 3),
+             (22750883079422934966181954039568885395604168260154104734000, 100, 100)]
+
+
+@pytest.mark.parametrize('expected, grid_width, grid_length', testcases)
+def test_unique_paths(expected, grid_width, grid_length):
+    assert expected == dp.Solution().uniquePaths(grid_width, grid_length)
+
+
+@pytest.mark.parametrize('expected, grid_width, grid_length', testcases)
+def test_unique_paths_rotate_grid(expected, grid_width, grid_length):
+    assert expected == dp.Solution().uniquePaths(grid_length, grid_width)
