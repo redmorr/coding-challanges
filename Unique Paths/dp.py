0f155560908841e602aadc16d537af51dc367671
redmorr <19555512+redmorr@users.noreply.github.com>
2021-11-27 13:46:47 +0100

    Add Unique Paths II puzzle and its DP solution

--- Unique Paths/dp.py
+++ Unique Paths/dp.py
@@ -7,7 +7,6 @@ class Solution:
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
         return dp[m - 1][n - 1]
 
-
     # Redeclared method from above with better memory complexity to compare code
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [1 for _ in range(n)]
@@ -16,3 +15,25 @@ class Solution:
             for j in range(1, n):
                 dp[j] = dp[j] + dp[j - 1]
         return dp[n - 1]
+
+    def uniquePathsWithObstacles(self, obstacleGrid: [[int]]) -> int:
+        result = obstacleGrid[0]
+
+        for i, item in enumerate(obstacleGrid[0]):
+            if item == 1:
+                for j in range(i, len(obstacleGrid[0])):
+                    result[j] = 0
+                break
+            else:
+                result[i] = 1
+
+        for row in obstacleGrid[1:]:
+            if row[0] == 1:
+                result[0] = 0
+            for i, col in enumerate(row[1:]):
+                if col == 1:
+                    result[i + 1] = 0
+                else:
+                    result[i + 1] = result[i + 1] + result[i]
+
+        return result[-1]
--- Unique Paths/test.py
+++ Unique Paths/test.py
@@ -4,6 +4,11 @@ import recursion
 import combination
 import dp
 
+
+def make_empty_grid(n, m):
+    return [[0 for _ in range(n)] for _ in range(m)]
+
+
 testcases = [(28, 3, 7),
              (2, 2, 2),
              (3, 3, 2),
@@ -11,14 +16,30 @@ testcases = [(28, 3, 7),
              (6, 3, 3),
              (22750883079422934966181954039568885395604168260154104734000, 100, 100)]
 
+testcases_obstacles = [[expected, make_empty_grid(n, m)] for expected, n, m in testcases]
+testcases_obstacles = testcases_obstacles + [(2, [[0, 0, 0], [0, 1, 0], [0, 0, 0]]),
+                                             (1, [[0, 1], [0, 0]]),
+                                             (0, [[1, 0]]),
+                                             (0, [[1], [0]]),
+                                             (0, [[1]]),
+                                             (13, [[0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]])]
+
 
 @pytest.mark.parametrize('expected, grid_width, grid_length', testcases)
-@pytest.mark.parametrize('solution_class', [recursion.Solution, combination.Solution, dp.Solution], ids=['Recursion', 'Combination', 'DP'])
+@pytest.mark.parametrize('solution_class', [recursion.Solution, combination.Solution, dp.Solution],
+                         ids=['Recursion', 'Combination', 'DP'])
 def test_unique_paths(solution_class, expected, grid_width, grid_length):
     assert expected == solution_class().uniquePaths(grid_width, grid_length)
 
 
 @pytest.mark.parametrize('expected, grid_width, grid_length', testcases)
-@pytest.mark.parametrize('solution_class', [recursion.Solution, combination.Solution, dp.Solution], ids=['Recursion', 'Combination', 'DP'])
+@pytest.mark.parametrize('solution_class', [recursion.Solution, combination.Solution, dp.Solution],
+                         ids=['Recursion', 'Combination', 'DP'])
 def test_unique_paths_rotate_grid(solution_class, expected, grid_width, grid_length):
     assert expected == solution_class().uniquePaths(grid_length, grid_width)
+
+
+@pytest.mark.parametrize('expected, grid', testcases_obstacles)
+@pytest.mark.parametrize('solution_class', [dp.Solution], ids=['DP'])
+def test_unique_paths_with_obstacles(solution_class, expected, grid):
+    assert expected == solution_class().uniquePathsWithObstacles(grid)
