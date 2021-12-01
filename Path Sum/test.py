661b37f97a1752d41bb9b37c91b1c0a25981413b
redmorr <19555512+redmorr@users.noreply.github.com>
2021-12-01 16:35:06 +0100

    Fix building tree from list of nodes by using BFS

--- Path Sum/test.py
+++ Path Sum/test.py
@@ -6,27 +6,23 @@ import dfs_stack
 import bfs_queue
 
 
-def grow_tree(list_of_nodes):
+def grow_tree(list_of_nodes: []):
     if len(list_of_nodes) == 0:
         return None
 
-    root = TreeNode(list_of_nodes[0])
-    layer = [root]
-    count = 1
+    root = TreeNode(list_of_nodes.pop(0))
+    queue = [root]
 
-    while count < len(list_of_nodes):
-        new_layer = []
-        for node in layer:
-            left = list_of_nodes[count:][0]
-            right = list_of_nodes[count:][1]
-            if left is not None:
-                node.left = TreeNode(left)
-                new_layer.append(node.left)
-            if right is not None:
-                node.right = TreeNode(right)
-                new_layer.append(node.right)
-            count += 2
-        layer = new_layer
+    while list_of_nodes:
+        node = queue.pop(0)
+        left = list_of_nodes.pop(0)
+        right = list_of_nodes.pop(0)
+        if left:
+            node.left = TreeNode(left)
+            queue.append(node.left)
+        if right:
+            node.right = TreeNode(right)
+            queue.append(node.right)
 
     return root
 
@@ -43,13 +39,13 @@ testcases2 = [([[5, 4, 11, 2], [5, 8, 4, 5]], 22, grow_tree([5, 4, 8, 11, None, 
 @pytest.mark.parametrize('expected, target_sum, root', testcases)
 @pytest.mark.parametrize('solution_class', [dfs_recursive.Solution, dfs_stack.Solution, bfs_queue.Solution],
                          ids=['DFS Recursive', 'DFS Stack', 'BFS Queue'])
-def test_has_path_sum(solution_class, expected, target_sum, root):
+def test_path_sum_1(solution_class, expected, target_sum, root):
     assert solution_class().hasPathSum(root, target_sum) == expected
 
 
 @pytest.mark.parametrize('expected, target_sum, root', testcases2)
 @pytest.mark.parametrize('solution_class', [dfs_recursive.Solution], ids=['DFS Recursive'])
-def test_has_path_sum_2(solution_class, expected, target_sum, root):
+def test_path_sum_2(solution_class, expected, target_sum, root):
     result = solution_class().pathSum(root, target_sum)
     for i, row in enumerate(expected):
         for j, _ in enumerate(row):
