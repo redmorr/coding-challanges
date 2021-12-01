ca515e0515514eb78e7933415e786653772b4784
redmorr <19555512+redmorr@users.noreply.github.com>
2021-12-01 15:11:22 +0100

    Add Path Sum II puzzle and its recursive DFS solution

--- Path Sum/test.py
+++ Path Sum/test.py
@@ -35,9 +35,22 @@ testcases = [(True, 22, grow_tree([5, 4, 8, 11, None, 13, 4, 7, 2, None, None, N
              (False, 5, grow_tree([1, 2, 3])),
              (False, 0, grow_tree([]))]
 
+testcases2 = [([[5, 4, 11, 2], [5, 8, 4, 5]], 22, grow_tree([5, 4, 8, 11, None, 13, 4, 7, 2, None, None, 5, 1])),
+              ([], 5, grow_tree([1, 2, 3])),
+              ([], 0, grow_tree([]))]
+
 
 @pytest.mark.parametrize('expected, target_sum, root', testcases)
-@pytest.mark.parametrize('solution_class', [dfs_recursive.Solution, dfs_stack.Solution, bfs_queue.Solution], ids=['DFS Recursive', 'DFS Stack', 'BFS Queue'])
+@pytest.mark.parametrize('solution_class', [dfs_recursive.Solution, dfs_stack.Solution, bfs_queue.Solution],
+                         ids=['DFS Recursive', 'DFS Stack', 'BFS Queue'])
 def test_has_path_sum(solution_class, expected, target_sum, root):
     assert solution_class().hasPathSum(root, target_sum) == expected
 
+
+@pytest.mark.parametrize('expected, target_sum, root', testcases2)
+@pytest.mark.parametrize('solution_class', [dfs_recursive.Solution], ids=['DFS Recursive'])
+def test_has_path_sum_2(solution_class, expected, target_sum, root):
+    result = solution_class().pathSum(root, target_sum)
+    for i, row in enumerate(expected):
+        for j, _ in enumerate(row):
+            assert expected[i][j] == result[i][j]
